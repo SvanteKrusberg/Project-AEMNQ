72383825094bac2f87047d5e594ce543a81a771d
SamNilsson172 <55138241+SamNilsson172@users.noreply.github.com>
2020-03-25 20:20:27 +0100

    added player

---
+++ Project AEMNQ/Assets/PlayerTest/ClickAndMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndMove : MonoBehaviour
+{
+    public Transform target;
+    public Camera cam;
+    public LayerMask walkMask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1)) //if right clicked
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, 500, walkMask)) //Send raycast from mouse to ground
+            {
+                target.position = hit.point; //move taget to clicked position
+            }
+            else print("bruh"); //if raycast didn't hit anything within the walkMask
+        }
+    }
+
+}
